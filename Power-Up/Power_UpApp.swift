55c43db724619149330171131059008f404d151e
Steve Miller <darkblueshyguy@aol.com>
2021-08-31 06:11:17 -0600

    This is the initial commit of what will be the most badass and dick-tickling workout app ever created

---
+++ Power-Up/Power_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Power_UpApp.swift
+//  Power-Up
+//
+//  Created by Steven Miller on 8/31/21.
+//
+
+import SwiftUI
+
+@main
+struct Power_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
